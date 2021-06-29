17d379b09c561c48023abb6dd496c70657996d69
Richard Yvarra <richard.yvarra@gmail.com>
2021-06-28 22:29:00 -0400

    added twitter

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLinkedin, faGithub, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin, faGithub, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons';
 import "./style.css"
 
 function Footer() {
@@ -12,6 +12,7 @@ function Footer() {
                         <FontAwesomeIcon icon={faLinkedin} size="2x" onClick={() => window.open("https://www.linkedin.com/in/richard-yvarra-205131205/")}/> 
                         <FontAwesomeIcon icon={faGithub} size="2x" onClick={() => window.open("https://github.com/Vinyl77")}/>
                         <FontAwesomeIcon icon={faInstagram} size="2x" onClick={() => window.open("https://www.instagram.com/archive77/?hl=en")}/>
+                        <FontAwesomeIcon icon={faTwitter} size="2x" onClick={() => window.open("https://twitter.com/RYDEV77")}/>
                 </p>
                 <p className="col-sm-12 copyright">
                     &copy; 2021 Richard Yvarra
